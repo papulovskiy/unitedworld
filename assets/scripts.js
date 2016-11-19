fea1161126deced8d77c804d29dfe64e42ad16e3
Alexey Papulovskiy <a@c17.nl>
2016-11-19 21:04:18 +0100

    Dataset listing, switching, fix of stale labels

--- assets/scripts.js
+++ assets/scripts.js
@@ -10,6 +10,14 @@ var uwo_create = function() {
             var country = geo_json.features[i];
             var id = country.properties['ISO2'];
             this.countries[id] = country;
+        }
+
+        this.create_underlying_boundaries(geo_json);
+    };
+
+    this.create_underlying_boundaries = function(geo_json) {
+        for(var id in this.countries) {
+            var country = this.countries[id];
             this.features[id] = L.geoJSON(country, {
                 style: function(feature) {
                     return {
@@ -26,23 +34,14 @@ var uwo_create = function() {
                 draggable: false,
                 zIndexOffset: 1000
             }).addTo(this.map);
+            var o = this;
+            this.map.on('movestart', function () {
+                o.map.removeLayer(o.labels[id]);
+            });
+            this.map.on('moveend', function () {
+                o.map.addLayer(o.labels[id]);
+            });
         }
-
-
-        // this.create_underlying_boundaries(geo_json);
-        this.draw_dataset('schengen');
-    };
-
-    this.create_underlying_boundaries = function(geo_json) {
-        this.geo = L.geoJSON(geo_json, {
-            style: function(feature) {
-                return {
-                    color: '#eee',
-                    stroke: false
-                };
-            }
-        }).addTo(this.map);
-        // console.log(this.geo);
     };
 
 
@@ -53,6 +52,7 @@ var uwo_create = function() {
             var dataset = this._datasets[i];
             this.datasets[dataset.url] = dataset;
         }
+        this.render_controls();
     };
 
     this.draw_dataset = function(url) {
@@ -66,22 +66,26 @@ var uwo_create = function() {
             color: '#000',
             stroke: false
         };
-        // TODO: implement existenche check
+
+        // TODO: implement existence check
         var dataset = this.datasets[url];
         var enabled = {};
         for(var i in dataset.countries) {
             enabled[dataset.countries[i]] = true;
         }
+
+        var bounds = L.latLngBounds();
         for(var country in this.features) {
             var feature = this.features[country];
             if(country in enabled) {
-                console.log('Enabling');
                 feature.setStyle(style_enabled);
+                bounds.extend(feature.getBounds());
             } else {
                 feature.setStyle(style_disabled);
             }
             // console.log(i);
         }
+        this.map.flyToBounds(bounds);
     };
 
     this.load_boundaries = function() {
@@ -97,6 +101,15 @@ var uwo_create = function() {
                 .catch(function(err) {
                     // TODO: implement error handling
                 });
+        } else {
+            // TODO: use XMLHttpRequest
+        }
+
+    };
+    this.load_datasets = function() {
+        if(self.fetch) {
+            console.debug('Loading datasets');
+            var o = this;
             fetch('data/datasets.json')
                 .then(function(response) {
                     response.json().then(function(data) {
@@ -109,8 +122,28 @@ var uwo_create = function() {
         } else {
             // TODO: use XMLHttpRequest
         }
+    };
 
-        console.log(this);
+    this.render_controls = function() {
+        var c = document.getElementById('controls-container');
+        var list = '';
+        console.log(this.datasets);
+        for(var id in this.datasets) {
+            console.debug(id);
+            var dataset = this.datasets[id];
+            list += '<li><a href="#" data-dataset-id="' + id + '">' + dataset.name + '</a></li>';
+        }
+        c.innerHTML = '<ul>' + list + '</ul>';
+
+        var o = this;
+        c.onclick = function(e) {
+            console.log(e);
+            e.preventDefault();
+            var url = e.target.getAttribute('data-dataset-id');
+            if(url) {
+                o.draw_dataset(url);
+            }
+        };
     };
 
 
@@ -118,10 +151,12 @@ var uwo_create = function() {
 
         // https://switch2osm.org/using-tiles/getting-started-with-leaflet/
 
-        this.map = L.map('map_container');
+        this.map = L.map('map-container');
         this.map.setView(new L.LatLng(51.3, 0.7), 2);
 
+
         this.load_boundaries();
+        this.load_datasets();
     };
 
 
