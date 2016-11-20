345cacb01552da54333f0c06fb36f00f1792b8df
Alexey Papulovskiy <a@c17.nl>
2016-11-20 14:31:57 +0100

    #3 Error handling on loading datasets

--- assets/scripts.js
+++ assets/scripts.js
@@ -84,28 +84,32 @@ var uwo_create = function() {
 
     this.adjust_to_bounds = function(b2) {
         var b1 = this.map.getBounds();
-        var diff_west = Math.abs(( (-180 - b2.getWest()) - (-180 - b1.getWest()))/(-180 - b1.getWest())),
-            diff_east = Math.abs(( (180 - b2.getEast()) - (180 - b1.getEast()))/(180 - b1.getEast())),
-            diff_north = Math.abs(( (90 - b2.getNorth()) - (90 - b1.getNorth()))/(90 - b1.getNorth())),
-            diff_south = Math.abs(( (-90 - b2.getSouth()) - (-90 - b1.getSouth()))/(-90 - b1.getSouth()));
+        var diff_west = Math.abs(((-180 - b2.getWest()) - (-180 - b1.getWest())) / (-180 - b1.getWest())),
+            diff_east = Math.abs(((180 - b2.getEast()) - (180 - b1.getEast())) / (180 - b1.getEast())),
+            diff_north = Math.abs(((90 - b2.getNorth()) - (90 - b1.getNorth())) / (90 - b1.getNorth())),
+            diff_south = Math.abs(((-90 - b2.getSouth()) - (-90 - b1.getSouth())) / (-90 - b1.getSouth()));
         // TODO: take zoom level into consideration or make these calculations better
-        if(Math.max(diff_west, diff_east, diff_north, diff_south) >= 2 || !b1.contains(b2)) {
+        if (Math.max(diff_west, diff_east, diff_north, diff_south) >= 2 || !b1.contains(b2)) {
             this.map.flyToBounds(b2);
         }
     };
 
     this.load_boundaries = function() {
-        if(self.fetch) {
+        if (self.fetch) {
             console.debug('Loading boundaries');
             var o = this;
             fetch('data/boundaries.json')
                 .then(function(response) {
-                    response.json().then(function(data) {
-                        o.store_boundaries(data);
-                    });
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                            o.store_boundaries(data);
+                        });
+                    } else {
+                        o.load_error();
+                    }
                 })
                 .catch(function(err) {
-                    // TODO: implement error handling
+                    o.load_error();
                 });
         } else {
             // TODO: use XMLHttpRequest
@@ -113,28 +117,36 @@ var uwo_create = function() {
 
     };
     this.load_datasets = function() {
-        if(self.fetch) {
+        if (self.fetch) {
             console.debug('Loading datasets');
             var o = this;
             fetch('data/datasets.json')
                 .then(function(response) {
-                    response.json().then(function(data) {
-                        o.store_datasets(data);
-                    });
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                            o.store_datasets(data);
+                        });
+                    } else {
+                        o.load_error();
+                    }
                 })
                 .catch(function(err) {
-                    // TODO: implement error handling
+                    o.load_error();
                 });
         } else {
             // TODO: use XMLHttpRequest
         }
     };
 
+    this.load_error = function() {
+        document.getElementsByTagName('body')[0].className += ' error';
+    };
+
     this.render_controls = function() {
         var c = document.getElementById('controls-container');
         var list = '';
         console.log(this.datasets);
-        for(var id of Object.keys(this.datasets).sort()) {
+        for (var id of Object.keys(this.datasets).sort()) {
             console.debug(id);
             var dataset = this.datasets[id];
             list += '<li><a href="#" data-dataset-id="' + id + '">' + dataset.name + '</a></li>';
